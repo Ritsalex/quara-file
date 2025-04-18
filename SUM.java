3c2d93a084e71d453ec75074f928289c4d7e7485
delta <159249751+Ritsalex@users.noreply.github.com>
2025-04-18 23:07:20 +0530

    pushing java code

---
+++ SUM.java
@@ -0,0 +1,11 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 4;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
---
+++ javabasic.java
@@ -0,0 +1,5 @@
+public class javabasic{
+    public static void main(string args[]){
+        system.out.print("hello adrika!");
+    }
+}
